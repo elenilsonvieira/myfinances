ebb3ea5ad48ab86b61b7fdef297eda3398d91a17
Elenilson <elenilsonvieira@gmail.com>
2022-05-13 10:53:22 -0300

    Estruturado o login para pegar local ou SUAP

--- src/main/java/br/edu/ifpb/dac/myfinances/business/service/impl/LoginServiceImpl.java
+++ src/main/java/br/edu/ifpb/dac/myfinances/business/service/impl/LoginServiceImpl.java
@@ -1,6 +1,7 @@
 package br.edu.ifpb.dac.myfinances.business.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import br.edu.ifpb.dac.myfinances.business.service.LoginService;
@@ -13,7 +14,32 @@ public class LoginServiceImpl implements LoginService{
 	@Autowired
 	private SystemUserService service;
 	
+	@Value("app.logintype")
+	private String logintype;
+	
 	public SystemUser login(String email, String password) {
+		switch (logintype) {
+		case "suap":
+			return suapLogin(email, password);
+		case "local":
+			return localLogin(email, password);
+		default:
+			return localLogin(email, password);
+		}
+	}
+	
+	private SystemUser localLogin(String email, String password) {
+		SystemUser user = service.findByEmail(email);
+		
+		if(user == null || password == null || 
+				!user.getPassword().equals(password)) {
+			throw new IllegalArgumentException("Incorrect Email or Password");
+		}
+		
+		return user;
+	}
+	
+	private SystemUser suapLogin(String email, String password) {
 		SystemUser user = service.findByEmail(email);
 		
 		if(user == null || password == null || 
